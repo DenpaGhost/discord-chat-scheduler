ca02f60875cd7795ef8aea29472ddca191d399b3
Naohiro Okamoto <az.kinosaki@gmail.com>
2020-10-07 12:11:09 +0900

    [add]モデル追加

---
+++ src/api/app/Models/OAuth/DiscordAuth.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models\OAuth;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
+
+/**
+ * Class DiscordAuth
+ * @package App\Models\OAuth
+ *
+ * @property-read int $id
+ * @property-read string $state
+ * @property-read string $discord_oauth_state
+ * @property-read string $code_challenge
+ * @property-read $created_at
+ * @property-read $updated_at
+ */
+class DiscordAuth extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'state',
+        'discord_oauth_state',
+        'code_challenge'
+    ];
+
+    /**
+     * @param Builder $query
+     * @param string $state
+     * @return Builder
+     */
+    public function scopeState($query, $state)
+    {
+        return $query->where('state', $state);
+    }
+
+    /**
+     * @param Builder $query
+     * @param string $state
+     * @return Builder
+     */
+    public function scopeDiscordOAuthState($query, $state)
+    {
+        return $query->where('discord_oauth_state', $state);
+    }
+}
--- src/api/app/Models/OAuth/DiscordToken.php
+++ src/api/app/Models/OAuth/DiscordToken.php
@@ -4,23 +4,37 @@ namespace App\Models\OAuth;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 /**
  * Class DiscordToken
  * @package App\Models
  *
  * @property-read int $id
- * @property-read int $token_id
  * @property string $access_token
  * @property string $refresh_token
  * @property $expires_in
+ * @property-read $is_expires
  */
 class DiscordToken extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'access_token',
+        'refresh_token',
+        'expires_in'
+    ];
+    protected $appends = ['is_expires'];
+
     public function token()
     {
-        return $this->belongsTo(Token::class);
+        return $this->hasOne(Token::class);
+    }
+
+    public function getIsExpiresAttributes()
+    {
+        $date = new Carbon($this->expires_in);
+        return $date->lt(Carbon::now());
     }
 }
